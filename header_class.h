aea4ead9d2296fb02b4860179f53258b0c7071f5
hocine abderrahmane <95042350+Abderrahmanehocine@users.noreply.github.com>
2022-01-29 17:07:55 +0100

    Update header_class.h

--- header_class.h
+++ header_class.h
@@ -1,6 +1,5 @@
 #include "header_files.h"
 
-namespace HeaderCode{
   class Header{
    private:
     string htmlCode;
@@ -40,7 +39,7 @@ namespace HeaderCode{
       return "0";
     }
    public:
-    void choseHeader( vector<string> pageName ){
+    void chooseHeader( vector<string> pageName ){
       cout<<endl<<"Header 1:\n"
       "------------------------\n"
       "|        Picture       |\n"
@@ -63,26 +62,26 @@ namespace HeaderCode{
         cin>>pictureFileName;
         htmlCode+="   <tr>\n    <td class=\"headertd\" colspan=\""+intToString(pageName.size())+"\">\n"   
         "     <img class=\"headerimg\" src=\""+pictureFileName+"\"/>\n";
-        htmlCode+="    </td>\n   </tr>\n";
+        htmlCode+="     </td>\n    </tr>\n";
       }
       htmlCode+="   <tr>\n";
-      htmlCode+="    <td class=\"headertd\">\n<a class=\"headera\" href=\"""index.html\">\n"+pageName[0]+"\n    </td>\n";
+       htmlCode+="    <td class=\"headertd\">\n<a class=\"headera\" href=\"""index.html\">\n"+pageName[0]+"\n    </td>\n";
       for(int i=1 ; i<pageName.size() ; i++){
         htmlCode+="    <td class=\"headertd\">\n<a class=\"headera\" href=\""+pageName[i]+".html\">\n"+pageName[i]+"\n    </td>\n";
       }
       htmlCode+="   </tr>\n  </table>";
       cssCode="table.headertable{\n";
-      cssCode+="  width: 80%;\n";
-      cssCode+="  margin-left: auto;\n  margin-right: auto;\n}\n";
+      cssCode+=" width: 80%;\n";
+      cssCode+=" margin-left: auto;\n margin-right: auto;\n}\n";
       cssCode+="td.headertd{\n";
-      cssCode+="  border: 1px solid white;\n";
-      cssCode+="  color: white;\n  text-align: center;\n  font-size: 20px;\n}";
+      cssCode+=" border: 1px solid white;\n";
+      cssCode+=" color: white;\n text-align: center;\n font-size: 20px;\n}";
       cssCode+="a.headera{\n";
-      cssCode+="  text-decoration: none;\n  color: white;\n";
+      cssCode+=" text-decoration: none;\n color: white;\n";
       cssCode+="}\n";
       if(headerChose == 1){
         cssCode+="img.headerimg{\n";
-        cssCode+="  width: 100px\n  heignt: auto\n}";
+        cssCode+=" width: 100px\n heignt: auto\n}";
       }
     }
     string getHtml(){
@@ -92,4 +91,3 @@ namespace HeaderCode{
       return cssCode;
     }
   };
-}
